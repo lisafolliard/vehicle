8584cfe4dc36e3c6837bef0215b29d34320e4b00
Lisa Folliard & Chris Underwood <student@epicodus.com>
2015-08-12 15:18:18 -0700

    Add success.erb, vehicle.erb, vehicles_form.erb files, modify app.rb file

--- app.rb
+++ app.rb
@@ -11,3 +11,21 @@ get('/vehicles') do
   @vehicles = Vehicle.all()
   erb(:vehicles)
 end
+
+get('/vehicles/new') do
+  erb(:vehicles_form)
+end
+
+get('/vehicles/:id') do
+  @vehicle = Vehicle.find(params.fetch("id").to_i())
+  erb(:vehicle)
+end
+
+post('/vehicles') do
+  make = params.fetch("make")
+  model = params.fetch("model")
+  year = params.fetch("year")
+  @vehicle = Vehicle.new(make, model, year)
+  @vehicle.save()
+  erb(:success)
+end 
