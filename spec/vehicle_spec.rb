1a120bdcd0d7b777a7930dc0fed4ddeba27ee198
Lisa Folliard & Chris Underwood <student@epicodus.com>
2015-08-12 09:49:06 -0700

    create model method to pass unit testing spec

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -11,6 +11,11 @@ describe('Vehicle') do
     end
   end
 
-
-
+  describe('#model') do
+    it('returns the model of the vehicle') do
+      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      # test_vehicle.save()
+      expect(test_vehicle.model()).to(eq("Prius"))
+    end
+  end
 end
