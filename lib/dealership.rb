ba32f487f690484e800d0084885cdc6c9095e469
Lisa Folliard <lisaf37@gmail.com>
2015-08-15 17:30:02 -0700

    Add cars method to Dealership class, unit test spec successful

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -4,6 +4,7 @@ class Dealership
   define_method(:initialize) do |name|
     @name = name
     @id = @@dealerships.length().+(1)
+    @cars = []
   end
 
   define_method(:name) do
@@ -13,5 +14,24 @@ class Dealership
   define_method(:id) do
     @id
   end
+
+  define_method(:cars) do
+    @cars
+  end
+
+  define_method(:save) do
+    @@dealerships.push(self)
+  end
+
+  define_singleton_method(:all) do
+    @@dealerships
+  end
+
+  define_singleton_method(:clear) do
+    @@dealerships = []
+  end
+
+
+
 
 end
--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -2,6 +2,9 @@ require('rspec')
 require('dealership')
 
 describe(Dealership) do
+  before() do
+    Dealership.clear()
+  end
 
   describe('#name') do
     it('returns name of dealership') do
@@ -17,13 +20,34 @@ describe(Dealership) do
     end
   end
 
+  describe(".all") do
+  it("is empty at first") do
+    expect(Dealership.all()).to(eq([]))
+  end
+end
 
+describe("#save") do
+  it("adds a dealership to the array of saved dealerships") do
+    test_dealership = Dealership.new("Bob's Used Cars")
+    test_dealership.save()
+    expect(Dealership.all()).to(eq([test_dealership]))
+  end
+end
 
+describe(".clear") do
+    it("empties out all of the saved dealerships") do
+      Dealership.new("Bob's Used Cars").save()
+      Dealership.clear()
+      expect(Dealership.all()).to(eq([]))
+    end
+  end
 
-
-
-
-
+  describe('#cars') do
+    it('initially returns an empty array of cars for the dealership') do
+      test_dealership = Dealership.new("Bob's Cars")
+      expect(test_dealership.cars()).to(eq([]))
+    end
+  end
 
 
 
